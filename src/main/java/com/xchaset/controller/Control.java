39b588f1df7cbaf190371626a8220037bdf8121d
xchaset <943314903@qq.com>
2017-11-26 21:21:05 +0800

    change the control.java

--- src/main/java/com/xchaset/controller/Control.java
+++ src/main/java/com/xchaset/controller/Control.java
@@ -3,13 +3,19 @@ package com.xchaset.controller;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+
+
 import com.xchaset.bean.UserInfo;
+import com.xchaset.dao.AuthoritiesMapper;
 import com.xchaset.dao.UserInfoMapper;
 import com.xchaset.dto.BaseResponse;
 
@@ -17,6 +23,9 @@ import com.xchaset.dto.BaseResponse;
 public class Control {
 	@Autowired
 	private UserInfoMapper usersMapper;
+	
+	@Autowired
+	private AuthoritiesMapper authoritiesMapper;
 
 	@RequestMapping("/user/{username}")
 	@ResponseBody
@@ -28,6 +37,8 @@ public class Control {
 			baseResponse.setRetMsg("无此用户");
 			return baseResponse;
 		}
+		List<GrantedAuthority>  authList = authoritiesMapper.getByUsername(username);
+		users.setAuthorities(authList);
 		baseResponse.setRetCode("00000");
 		baseResponse.setRetMsg("查询成功");
 		baseResponse.setData(users);
